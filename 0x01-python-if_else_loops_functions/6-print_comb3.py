54773393119c3dcd0efce073868f5c0c31c94eda
DESIREAK001 <desirengonzikabagyo.gmail.com>
2023-11-28 18:32:31 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end=", " if tens_digit != 8 or ones_digit != 9 else "\n")
+for tens in range(10):
+    for ones in range(tens + 1, 10):
+        print("{:d}{:d}".format(tens, ones), end=", " if tens != 8 or ones != 9 else "\n")
