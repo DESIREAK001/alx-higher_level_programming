250b1aaa2d75b72603b26356d3b5c88e7d879d37
DESIREAK001 <desirengonzikabagyo.gmail.com>
2023-11-28 18:35:26 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for tens in range(10):
-    for ones in range(tens + 1, 10):
-        print("{:d}{:d}".format(tens, ones), end=", " if tens != 8 or ones != 9 else "\n")
+for p in range(10):
+    for q in range(p + 1, 10):
+        print("{:d}{:d}".format(p, q), end=", " if p != 8 or q != 9 else "\n")
