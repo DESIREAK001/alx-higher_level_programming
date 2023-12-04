cd3aa65e199017c2fad895b65b2725f871b65c2d
DESIREAK001 <desirengonzikabagyo.gmail.com>
2023-12-04 08:48:58 -0800

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    # Check if the ASCII value of the character is within the range of lowercase letters
-    return 97 <= ord(c) <= 122
-
-# Test the function
-print(islower('a'))  # True
-print(islower('A'))  # False
-print(islower('1'))  # False
+    return ord('c') >= ord('a') and ord('c') <= ord('z')
