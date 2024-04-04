3d784ae5caa9d1b38cd4ecc5dd854d029d7df570
DESIREAK001 <desirengonzikabagyo@gmail.com>
2024-04-04 09:30:29 -0700

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv[1] === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(process.argv[1]);
+  console.log(process.argv[2]);
 }
